b55cb6ce13660e3ae5b680f7f1a2d4f4833b44b5
mina-gwak <mina.gwak@kakao.com>
2022-08-20 22:49:23 +0900

    feat: Show widget when sidebar app is clicked

--- main.ts
+++ main.ts
@@ -4,12 +4,33 @@
 
 import "zep-script";
 
-ScriptApp.showCenterLabel("Hello World");
+ScriptApp.onSidebarTouched.Add(function(p) {
+  if (p.role === 3000 || p.role === 3001) {
+    p.tag.widtget = p.showWidget('daystamp.html', 'sidebar', 480, 400);
+  } else {
+    p.tag.widtget = p.showWidget('daystamp-user.html', 'sidebar', 480, 400);
+  }
 
-let zepLogo = ScriptApp.loadSpritesheet("zep_logo.png");
+  p.tag.widget.onMessage.Add(function (player, data) {
+    if (data.type == "close") {
+      player.showCenterLabel("위젯이 닫혔습니다.");
+      player.tag.widget.destroy();
+      player.tag.widget = null;
+    }
+  });
+})
 
-ScriptMap.putObject(0, 0, zepLogo, { overlap: true });
+// 플레이어가 입장 할 때 동작하는 함수
+ScriptApp.onJoinPlayer.Add(function (p) {
+  p.tag = {
+    widget: null,
+  };
+});
 
-ScriptApp.onDestroy.Add(function () {
-  ScriptMap.clearAllObjects();
+// 플레이어가 퇴장 할 때 동작하는 함수
+ScriptApp.onLeavePlayer.Add(function (p) {
+  if (p.tag.widget) {
+    p.tag.widget.destroy();
+    p.tag.widget = null;
+  }
 });
